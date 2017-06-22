362cb9828edac118e3b6429a440a777c50e4bd0e
Paulo Lemus <paulolemus14@gmail.com>
2017-06-22 12:17:52 -1000

    Can now get fragments from FragmentPagerAdapter using constant fragment indices

--- app/src/main/java/com/bah/iotsap/MainActivity.java
+++ app/src/main/java/com/bah/iotsap/MainActivity.java
@@ -2,10 +2,12 @@ package com.bah.iotsap;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.util.SparseArray;
 
 /**
  * This activity is basically a container for various fragments. It consists
@@ -17,7 +19,11 @@ public class MainActivity extends FragmentActivity {
     private static final String TAG = "MainActivity";
     private static final int NUM_FRAGMENTS = 5;
     private static final int START_INDEX   = 1;
-
+    private static final int PREF_INDEX    = 0;
+    private static final int MAP_INDEX     = 1;
+    private static final int BT_INDEX      = 2;
+    private static final int BLE_INDEX     = 3;
+    private static final int NFC_INDEX     = 4;
 
     PagerAdapter pagerAdapter;
     ViewPager viewPager;
@@ -42,21 +48,31 @@ public class MainActivity extends FragmentActivity {
 
     private static class PagerAdapter extends FragmentPagerAdapter {
 
+        SparseArray<Fragment> registeredFragments = new SparseArray<>(NUM_FRAGMENTS);
+
         public PagerAdapter(android.app.FragmentManager fm) {
             super(fm);
             Log.i(TAG, "PagerAdapter: constructor");
         }
+
+        /**
+         * Instantiates fragments for each position and stores them in a SparseArray
+         * for later retrieval.
+         * @param position
+         * @return fragment belonging to position
+         */
         @Override
         public Fragment getItem(int position) {
             Log.i(TAG, "PagerAdapter: getItem, position = " + position);
             switch(position) {
-                case 0:  return new SettingsFragment();
-                case 1:  return new MapFragment();
-                case 2:  return new TaskFragment(); // Bluetooth fragment
-                case 3:  return new TaskFragment(); // BLE fragment
-                case 4:  return new TaskFragment(); // NFC fragment
-                default: return null;
+                case PREF_INDEX: registeredFragments.put(position, new SettingsFragment()); break;
+                case MAP_INDEX : registeredFragments.put(position, new MapFragment()); break;
+                case BT_INDEX  :
+                case BLE_INDEX :
+                case NFC_INDEX : registeredFragments.put(position, new TaskFragment()); break;
+                default: break;
             }
+            return registeredFragments.get(position);
         }
 
         /**
@@ -66,6 +82,18 @@ public class MainActivity extends FragmentActivity {
         @Override
         public int getCount() {
             return NUM_FRAGMENTS;
+        }
+
+        /**
+         * Use to retrieve a fragment at a particular position.
+         * Usage: MapFragment frag = (MapFragment) pagerAdapter.getFragmentAt(NFC_INDEX);
+         * @param position
+         * @return Fragment
+         */
+        @Nullable
+        public Fragment getFragmentAt(int position) {
+            Log.i(TAG, "PagerAdapter: getFragmentAt(" + position + ")");
+            return registeredFragments.get(position);
         }
     }
 }
