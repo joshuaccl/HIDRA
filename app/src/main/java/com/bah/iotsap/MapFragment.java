50b948b308ec7485eab6f09b116909a39a841811
Paulo Lemus <Lemus_Paulo@bah.com>
2017-07-25 14:46:06 -1000

    Added skeleton HUD elements to the MapFragment. Modified the viewPager in MainActivity to control changing between pages.

--- app/src/main/java/com/bah/iotsap/MapFragment.java
+++ app/src/main/java/com/bah/iotsap/MapFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.mapbox.mapboxsdk.maps.MapView;
 
@@ -18,7 +19,39 @@ import com.mapbox.mapboxsdk.maps.MapView;
 public class MapFragment extends Fragment {
 
     private static final String TAG = "MapFragment";
+
     private MapView mapView;
+    private Button moreBtn;     // Button to slide left linear layout down
+    private Button nfcBtn;      // Transition to NFC fragment
+    private Button rfidBtn;     // Transition to RFID fragment
+    private Button prefBtn;     // Transition to preferences fragment
+    private boolean moreBtnClicked = false;
+
+    private View.OnClickListener moreBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.i(TAG, "moreBtn clicked");
+        }
+    };
+    private View.OnClickListener nfcBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.i(TAG, "nfcBtn clicked");
+        }
+    };
+    private View.OnClickListener rfidBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.i(TAG, "rfidBtn clicked");
+        }
+    };
+    private View.OnClickListener prefBtnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.i(TAG, "prefBtn clicked");
+            ((MainActivity) getActivity()).viewPager.setCurrentItem(MainActivity.PREF_INDEX);
+        }
+    };
 
     public static MapFragment newInstance() {
         return new MapFragment();
@@ -31,6 +64,15 @@ public class MapFragment extends Fragment {
         View fragmentLayout = inflater.inflate(R.layout.fragment_map, container, false);
         mapView = (MapView) fragmentLayout.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+
+        moreBtn = (Button) fragmentLayout.findViewById(R.id.map_left_more_button);
+        nfcBtn  = (Button) fragmentLayout.findViewById(R.id.map_left_nfc_button);
+        rfidBtn = (Button) fragmentLayout.findViewById(R.id.map_left_rfid_button);
+        prefBtn = (Button) fragmentLayout.findViewById(R.id.map_left_pref_button);
+        moreBtn.setOnClickListener(moreBtnListener);
+        nfcBtn .setOnClickListener(nfcBtnListener);
+        rfidBtn.setOnClickListener(rfidBtnListener);
+        prefBtn.setOnClickListener(prefBtnListener);
         return fragmentLayout;
     }
 
